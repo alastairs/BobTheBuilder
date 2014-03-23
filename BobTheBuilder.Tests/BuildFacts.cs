177b3db4ed267dfc4ae46199754d1219e25436f0
Alastair Smith <github@alastairsmith.me.uk>
2014-03-23 11:41:58 +0000

    Create an instance of the requested type.
    We get quite a lot of code out of this first test, because where
    previously there was nothing, there is now something.

---
+++ BobTheBuilder.Tests/BuildFacts.cs
@@ -0,0 +1,19 @@
+﻿using Xunit;
+
+namespace BobTheBuilder.Tests
+{
+    public class BuildFacts
+    {
+        [Fact]
+        public void CreateAnInstanceOfTheRequestedType()
+        {
+            var sut = A.BuilderFor<SampleType>();
+
+            var result = sut.Build();
+
+            Assert.IsAssignableFrom<SampleType>(result);
+        }
+
+        private class SampleType { }
+    }
+}
---
+++ BobTheBuilder/Builder.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Dynamic;
+
+namespace BobTheBuilder
+{
+    public class DynamicBuilder : DynamicObject
+    {
+        private readonly Type _destinationType;
+
+        internal DynamicBuilder(Type destinationType)
+        {
+            if (destinationType == null)
+            {
+                throw new ArgumentNullException("destinationType");
+            }
+
+            _destinationType = destinationType;
+        }
+
+        public object Build()
+        {
+            return Activator.CreateInstance(_destinationType);
+        }
+    }
+
+    public class A
+    {
+        public static DynamicBuilder BuilderFor<T>()
+        {
+            return new DynamicBuilder(typeof(T));
+        }
+    }
+}
