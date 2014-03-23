e6eeb134fb0a42621a853202f169ca7f55acd23f
Alastair Smith <github@alastairsmith.me.uk>
2014-03-23 11:56:04 +0000

    The factory method must return `dynamic`.
    This is to ensure the dynamic dispatch functionality is enabled.

--- BobTheBuilder.Tests/BuildFacts.cs
+++ BobTheBuilder.Tests/BuildFacts.cs
@@ -5,12 +5,13 @@ namespace BobTheBuilder.Tests
     public class BuildFacts
     {
         [Fact]
-        public void CreateAnInstanceOfTheRequestedType()
+        public void CreateADynamicInstanceOfTheRequestedType()
         {
             var sut = A.BuilderFor<SampleType>();
 
             var result = sut.Build();
 
+            Assert.IsAssignableFrom<dynamic>(result);
             Assert.IsAssignableFrom<SampleType>(result);
         }
 
