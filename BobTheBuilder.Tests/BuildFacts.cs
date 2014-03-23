11fa2a48b4430acbf8a6d9a7928da432a411a64e
Alastair Smith <github@alastairsmith.me.uk>
2014-03-23 13:03:37 +0000

    Move example usage tests to another fixture.

--- BobTheBuilder.Tests/BuildFacts.cs
+++ BobTheBuilder.Tests/BuildFacts.cs
@@ -5,37 +5,6 @@ namespace BobTheBuilder.Tests
     public class BuildFacts
     {
         [Fact]
-        public void Usage()
-        {
-            const string stringProperty = "expected value";
-            var expected = new SampleType
-            {
-                StringProperty = stringProperty
-            };
-
-            var built = A.BuilderFor<SampleType>()
-                            .WithStringProperty(stringProperty)
-                            .Build();
-
-            Assert.Equal(expected, built, new SampleTypeEqualityComparer());
-        }
-
-        [Fact]
-        public void UsageWithImplicitCast()
-        {
-            const string stringProperty = "expected value";
-            var expected = new SampleType
-            {
-                StringProperty = stringProperty
-            };
-
-            SampleType built = A.BuilderFor<SampleType>()
-                                    .WithStringProperty(stringProperty);
-
-            Assert.Equal(expected, built, new SampleTypeEqualityComparer());
-        }
-
-        [Fact]
         public void CreateADynamicInstanceOfTheRequestedType()
         {
             var sut = A.BuilderFor<SampleType>();
---
+++ BobTheBuilder.Tests/ExampleUsage.cs
@@ -0,0 +1,38 @@
+﻿using Xunit;
+
+namespace BobTheBuilder.Tests
+{
+    public class ExampleUsage
+    {
+        [Fact]
+        public void Usage()
+        {
+            const string stringProperty = "expected value";
+            var expected = new SampleType
+            {
+                StringProperty = stringProperty
+            };
+
+            var built = A.BuilderFor<SampleType>()
+                .WithStringProperty(stringProperty)
+                .Build();
+
+            Assert.Equal(expected, built, new SampleTypeEqualityComparer());
+        }
+
+        [Fact]
+        public void UsageWithImplicitCast()
+        {
+            const string stringProperty = "expected value";
+            var expected = new SampleType
+            {
+                StringProperty = stringProperty
+            };
+
+            SampleType built = A.BuilderFor<SampleType>()
+                .WithStringProperty(stringProperty);
+
+            Assert.Equal(expected, built, new SampleTypeEqualityComparer());
+        }
+    }
+}
