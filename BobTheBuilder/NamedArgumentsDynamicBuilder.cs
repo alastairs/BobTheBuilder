6bb1661af15ece72e0f34181c639dff3ad0f81ee
Alastair Smith <github@alastairsmith.me.uk>
2014-04-10 21:07:07 +0100

    Add NamedArgumentsDynamicBuilder.
    This is a Decorator for DynamicBuilder to store the named arguments parsing.

---
+++ BobTheBuilder/NamedArgumentsDynamicBuilder.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Dynamic;
+
+namespace BobTheBuilder
+{
+    public class NamedArgumentsDynamicBuilder<T> : DynamicObject, IDynamicBuilder<T> where T : class
+    {
+        private readonly IDynamicBuilder<T> wrappedBuilder;
+
+        public NamedArgumentsDynamicBuilder(IDynamicBuilder<T> wrappedBuilder)
+        {
+            if (wrappedBuilder == null)
+            {
+                throw new ArgumentNullException("wrappedBuilder");
+            }
+
+            this.wrappedBuilder = wrappedBuilder;
+        }
+
+        public override bool TryInvokeMember(InvokeMemberBinder binder, object[] args, out object result)
+        {
+            return wrappedBuilder.TryInvokeMember(binder, args, out result);
+        }
+
+        public T Build()
+        {
+            return wrappedBuilder.Build();
+        }
+    }
+}
