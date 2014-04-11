30a5a4abed9fb2519b6e72308977f624484b5b57
Alastair Smith <github@alastairsmith.me.uk>
2014-04-11 21:03:52 +0100

    Add support for named argument parsing.
    The argument name munging is not pretty and needs improving (and perhaps
    breaking out into another class?), but it does at least currently work.

--- BobTheBuilder/NamedArgumentsDynamicBuilder.cs
+++ BobTheBuilder/NamedArgumentsDynamicBuilder.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Dynamic;
+using System.Linq;
 
 namespace BobTheBuilder
 {
@@ -26,7 +27,19 @@ namespace BobTheBuilder
 
         public override bool TryInvokeMember(InvokeMemberBinder binder, object[] args, out object result)
         {
+            if (binder.Name == "With")
+            {
+                ParseNamedArgumentValues(binder.CallInfo, args);
+            }
+
             return wrappedBuilder.TryInvokeMember(binder, args, out result);
+        }
+
+        private void ParseNamedArgumentValues(CallInfo callInfo, object[] args)
+        {
+            var argumentName = callInfo.ArgumentNames.First();
+            argumentName = argumentName.First().ToString().ToUpper() + argumentName.Substring(1);
+            argumentStore.SetMemberNameAndValue(argumentName, args.First());
         }
 
         public T Build()
