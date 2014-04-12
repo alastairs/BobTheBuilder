cace053a648fa0ffbfb74d9334208d2d0fcd8486
Alastair Smith <github@alastairsmith.me.uk>
2014-04-12 14:43:28 +0100

    Add new `CompositeParser`.
    This will wrap up all the different parser types and try each of them in
    turn to get a result.

---
+++ BobTheBuilder/Syntax/CompositeParser.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+
+namespace BobTheBuilder.Syntax
+{
+    class CompositeParser : IParser
+    {
+        private readonly IEnumerable<IParser> wrappedParsers;
+
+        public CompositeParser(params IParser[] wrappedParsers)
+        {
+            if (wrappedParsers == null)
+            {
+                throw new ArgumentNullException("wrappedParsers");
+            }
+
+            this.wrappedParsers = wrappedParsers;
+        }
+
+        public bool Parse(InvokeMemberBinder binder, object[] callArguments)
+        {
+            return wrappedParsers.Any(p => p.Parse(binder, callArguments));
+        }
+    }
+}
