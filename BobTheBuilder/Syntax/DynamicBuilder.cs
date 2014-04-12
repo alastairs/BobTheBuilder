e8fb6859789491e76e2fd8abf0a1e0fde7d4ee66
Alastair Smith <github@alastairsmith.me.uk>
2014-04-12 14:23:48 +0100

    Implement IParser on method syntax builder.

--- BobTheBuilder/Syntax/DynamicBuilder.cs
+++ BobTheBuilder/Syntax/DynamicBuilder.cs
@@ -4,22 +4,23 @@ using BobTheBuilder.ArgumentStore;
 
 namespace BobTheBuilder.Syntax
 {
-    public class DynamicBuilder<T> : DynamicBuilderBase<T> where T: class
+    public class DynamicBuilder<T> : DynamicBuilderBase<T>, IParser where T: class
     {
         public DynamicBuilder(IArgumentStore argumentStore) : base(argumentStore) { }
 
         public override bool InvokeBuilderMethod(InvokeMemberBinder binder, object[] args, out object result)
         {
-            ParseMembersFromMethodName(binder, args);
+            Parse(binder, args);
 
             result = this;
             return true;
         }
 
-        private void ParseMembersFromMethodName(InvokeMemberBinder binder, object[] args)
+        public bool Parse(InvokeMemberBinder binder, object[] args)
         {
             var memberName = binder.Name.Replace("With", "");
             argumentStore.SetMemberNameAndValue(memberName, args[0]);
+            return true;
         }
     }
 }
