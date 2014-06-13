6cb2569c3e485db83f54fb82da579300c2bd3e88
Alastair Smith <github@alastairsmith.me.uk>
2014-06-13 20:23:46 +0100

    Add immutable sample types.

---
+++ BobTheBuilder.Tests/ImmutableAuthor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace BobTheBuilder.Tests
+{
+    internal class ImmutableAuthor
+    {
+        private readonly string name;
+        private readonly DateTime startDate;
+
+        public ImmutableAuthor(string name, DateTime startDate)
+        {
+            this.name = name;
+            this.startDate = startDate;
+        }
+
+        public string Name
+        {
+            get { return name; }
+        }
+
+        public DateTime StartDate
+        {
+            get { return startDate; }
+        }
+    }
+}
---
+++ BobTheBuilder.Tests/ImmutableBlogPost.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BobTheBuilder.Tests
+{
+    internal class ImmutableBlogPost
+    {
+        private readonly string title;
+        private readonly ImmutableAuthor author;
+        private readonly string content;
+        private readonly DateTime timestamp;
+        private readonly IEnumerable<string> tags;
+
+        public ImmutableBlogPost(string title, ImmutableAuthor author, string content, DateTime timestamp, IEnumerable<string> tags)
+        {
+            this.title = title;
+            this.author = author;
+            this.content = content;
+            this.timestamp = timestamp;
+            this.tags = tags;
+        }
+
+        public string Title
+        {
+            get { return title; }
+        }
+
+        public ImmutableAuthor Author
+        {
+            get { return author; }
+        }
+
+        public string Content
+        {
+            get { return content; }
+        }
+
+        public DateTime Timestamp
+        {
+            get { return timestamp; }
+        }
+
+        public IEnumerable<string> Tags
+        {
+            get { return tags; }
+        }
+    }
+}
